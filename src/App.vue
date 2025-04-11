aa62366b11280a96313b363ce465feed354cac64
Derick Castillo <derick.castillo@daveramsey.com>
2025-04-11 06:22:33 -0500

    Add css/image files to render socks

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
+  <div class="nav-bar"></div>
   <div class="product-display">
     <div class="product-container">
+      <div class="product-image">
+        <img v-bind:src="socksGreenImage" />
+      </div>
       <div class="product-info">
         <h1>{{ product }}</h1>
         <p>{{ description }}</p>
@@ -11,6 +15,7 @@
 
 <script setup>
 import { ref } from 'vue';
+import socksGreenImage from './assets/images/socks_green.jpeg';
 
 const product = ref('Socks')
 const description = ref('These socks are made from 100% cotton.')
